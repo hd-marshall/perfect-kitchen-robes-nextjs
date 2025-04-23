a8ea1ea2486dbd251cdff69eba73cb2562a3dbfc
Harry Marshall <harryyymarshall@gmail.com>
2025-04-23 19:10:13 +1000

    thank you page and dir

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/Button';
+
+export default function ThankYouPage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-zinc-800 text-center px-4">
+        <h1 className="text-6xl md:text-8xl font-bold text-white">
+          Thank You
+        </h1>
+        <p className="text-xl md:text-2xl text-white mb-6">
+          From the crew of Perfect Kitchen & Robes
+        </p>
+        
+        <p className="text-lg md:text-xl text-white mb-8 mx-auto w-[90%] md:w-[70%]">
+          We've received your enquiry and will be in touch shortly. We look forward to helping you transform your space.
+        </p>
+        
+        <div>
+          <Link href="/">
+            <Button 
+              intent="primary" 
+              size="lg"
+            >
+              Return to Home
+            </Button>
+          </Link>
+        </div>
+    </div>
+  );
+}
