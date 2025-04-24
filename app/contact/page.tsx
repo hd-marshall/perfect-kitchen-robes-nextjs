2a16ad9f6eeab52b1dbb568139e3f7708b41e495
Harry Marshall <harryyymarshall@gmail.com>
2025-04-24 13:48:45 +1000

    contact page change

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -44,14 +44,14 @@ export default function ContactPage() {
             
             {/* Phone - Now clickable */}
             <div className="flex items-center">
-                <a href="tel:0433537057" className="flex items-center group">
+                <a href="tel:0393577204" className="flex items-center group">
                   <div className="h-16 w-16 rounded-full border-2 border-zinc-800 flex items-center justify-center flex-shrink-0 group-hover:bg-zinc-100 transition-colors">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7 text-zinc-800">
                         <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd" />
                     </svg>
                   </div>
                   <div className="ml-6">
-                    <p className="text-xl font-medium text-gray-800 group-hover:text-gray-600 transition-colors">0433 537 057</p>
+                    <p className="text-xl font-medium text-gray-800 group-hover:text-gray-600 transition-colors">03 9357-7204</p>
                     <p className="text-gray-500 text-sm group-hover:underline">Click to call</p>
                   </div>
                 </a>
